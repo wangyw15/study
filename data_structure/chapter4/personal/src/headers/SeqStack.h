54d6657d36923353279367aac375e4b707843fae
wangyw15 <wangyanwei15@outlook.com>
2022-12-11 23:20:29 +0800

    做完第四章第一个

---
+++ data_structure/chapter4/personal/src/headers/SeqStack.h
@@ -0,0 +1,159 @@
+#ifndef SEQSTACK
+#define SEQSTACK
+
+#include <ostream>
+#include <string>
+#include <functional>
+
+template <typename T>
+class SeqStack
+{
+protected:
+    T* _Data;
+    long _Capacity;
+    long _Top;
+    std::function<std::ostream&(std::ostream&, SeqStack<T>&)> _Printer = _DefaultPrinter;
+
+    // stackfull
+    void _Expand()
+    {
+        T* newData = new T[_Capacity * 2];
+        for (long i = 0; i < _Top + 1; i++)
+        {
+            newData[i] = _Data[i];
+        }
+        delete [] _Data;
+        _Data = newData;
+        _Capacity *= 2;
+    }
+    
+    static std::ostream& _DefaultPrinter(std::ostream& out, SeqStack<T>& stack)
+    {
+        out << std::string("[ ");
+        for (long i = stack._Top; i >= 0; i--)
+        {
+            out << (stack._Data[i]) << (i == 0 ? "" : ", ");
+        }
+        out << std::string(" ]");
+        return out;
+    }
+
+public:
+    SeqStack()
+    {
+        _Data = new T[100];
+        _Capacity = 100;
+        _Top = -1;
+    }
+
+    SeqStack(const SeqStack<T> &stack)
+    {
+        *this = stack;
+    }
+
+    SeqStack<T> &operator=(const SeqStack<T> &stack)
+    {
+        if (&stack != this)
+        {
+            _Top = stack._Top;
+            _Printer = stack._Printer;
+            _Capacity = stack._Capacity;
+            _Data = new T[_Capacity];
+
+            for (long i = _Top; i >= 0; i--)
+            {
+                _Data[i] = stack._Data[i];
+            }
+        }
+        return *this;
+    }
+
+    virtual ~SeqStack()
+    {
+        delete[] _Data;
+        _Capacity = 0;
+        _Top = -1;
+    }
+
+    void Clear()
+    {
+        delete[] _Data;
+        _Data = new T[_Capacity];
+        _Top = -1;
+    }
+
+    bool IsEmpty() const
+    {
+        return _Top == -1;
+    }
+
+    long Length() const
+    {
+        return _Top + 1;
+    }
+
+    void Traverse(std::function<void(const T)> func) const
+    {
+        for (long i = _Top; i >= 0; i--)
+        {
+            func(_Data[i]);
+        }
+    }
+
+    void Push(const T item)
+    {
+        if (_Top + 1 >= _Capacity)
+        {
+            _Expand();
+        }
+        _Data[++_Top] = item;
+    }
+
+    T Pop()
+    {
+        if (_Top - 1 >= 0)
+        {
+            return _Data[_Top--];
+        }
+        else
+        {
+            throw "Index out of range";
+        }
+    }
+
+    T &Top() const
+    {
+        if (_Top >= 0)
+        {
+            return _Data[_Top];
+        }
+        else
+        {
+            throw "Empty stack";
+        }
+    }
+
+    T &operator[](int n) const
+    {
+        if (n <= _Top && n >= 0)
+        {
+            return _Data[n];
+        }
+        else
+        {
+            throw "Index out of range";
+        }
+    }
+
+    void SetPrinter(std::function<std::ostream&(std::ostream&, SeqStack<T>&)> printer = _DefaultPrinter)
+    {
+        _Printer = printer;
+    }
+
+    friend std::ostream& operator<<(std::ostream &out, SeqStack<T> &stack)
+    {
+        return stack._Printer(out, stack);
+    }
+};
+
+#endif
---
+++ data_structure/chapter4/personal/src/seqstack.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <ios>
+#include "headers/SeqStack.h"
+
+using namespace std;
+
+struct Car
+{
+    string Plate;
+    int Year;
+    int Month;
+    int Day;
+    int Hour;
+    int Minute;
+    int Second;
+
+    bool operator<(const Car &other)
+    {
+        if (Year != other.Year)
+        {
+            return Year < other.Year;
+        }
+        else if (Month != other.Month)
+        {
+            return Month < other.Month;
+        }
+        else if (Day != other.Day)
+        {
+            return Day < other.Day;
+        }
+        else if (Hour != other.Hour)
+        {
+            return Hour < other.Hour;
+        }
+        else if (Minute != other.Minute)
+        {
+            return Minute < other.Minute;
+        }
+        else if (Second != other.Second)
+        {
+            return Second < other.Second;
+        }
+        else
+        {
+            return Plate < other.Plate;
+        }
+    }
+    
+    bool operator>(const Car &other)
+    {
+        return !(*this < other);
+    }
+
+    friend ostream &operator<<(ostream& out, Car &car)
+    {
+        out << "Plate: " << car.Plate << "; Enter time: "
+            << car.Year << "/" << car.Month << "/" << car.Day << " "
+            << car.Hour << ":" << car.Minute << ":" << car.Second;
+        return out;
+    }
+
+    friend istream &operator>>(istream& in, Car &car)
+    {
+        in >> car.Plate
+            >> car.Year >> car.Month >> car.Day
+            >> car.Hour >> car.Minute >> car.Second;
+        return in;
+    }
+};
+
+int main()
+{
+    // (1)
+    Car tempCar;
+    SeqStack<Car> cars;
+    cout << "the amount of data: ";
+    int count = 0;
+    cin >> count;
+    cout << "data:" << endl;
+    for (auto i = 0; i < count; i++)
+    {
+        cin >> tempCar;
+        cars.Push(tempCar);
+    }
+    cars.Traverse([] (Car item) { cout << item << endl; });
+    cout << endl;
+
+    // (2)
+    SeqStack<int> stack;
+    for (auto i = 0; i < 200; i++)
+    {
+        stack.Push(i);
+    }
+    cout << stack << endl;
+    return 0;
+}
