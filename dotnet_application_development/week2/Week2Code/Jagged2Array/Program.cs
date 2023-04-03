a61616ede80212a90dec7db91fac58d69da35ca0
wangyw15 <wangyanwei2012@sina.com>
2023-04-03 17:42:46 +0800

    换到新版.NET

---
+++ dotnet_application_development/week2/Week2Code/Jagged2Array/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter 7 words: ");
+var words = Console.ReadLine().Split(' ');
+
+var strJaggedArray = new string[2][];
+strJaggedArray[0] = new string[] { words[0], words[1], words[2], words[3] };
+strJaggedArray[1] = new string[] { words[4], words[5], words[6] };
+
+var strArray = new List<string>();
+for (int i = 0; i < strJaggedArray[0].Length; i++)
+{
+    for (int j = 0; j < strJaggedArray[1].Length; j++)
+    {
+        strArray.Add(strJaggedArray[0][i] + "&" + strJaggedArray[1][j]);
+    }
+}
+
+foreach (var item in strArray)
+{
+    Console.WriteLine(item);
+}
---
+++ dotnet_application_development/week2/Week2Code/ObjectEquality/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 1, b = 1;
+float f = 1.0f;
+Console.WriteLine($"a == b: {a == b}");
+Console.WriteLine($"Object.Equals(a, b): {Object.Equals(a, b)}");
+
+Console.WriteLine($"a == f: {a == f}");
+Console.WriteLine($"Object.Equals(a, f): {Object.Equals(a, f)}");
+
+object t1 = new object(), t2 = new object();
+Console.WriteLine($"t1 == t2: {t1 == t2}");
+Console.WriteLine($"Object.Equals(t1, t2): {Object.Equals(t1, t2)}");
