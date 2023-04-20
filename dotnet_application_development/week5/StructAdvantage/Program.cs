37fd689ebe16a8ddbf8b0f910b24ec29cce7217c
wangyw15 <wangyanwei15@outlook.com>
2023-04-20 19:48:55 +0800

    dotnet第五周实验三

--- dotnet_application_development/week5/StructAdvantage/Program.cs
+++ dotnet_application_development/week5/StructAdvantage/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var points = new Point[15];
+var spoints = new PointStruct[15];
+
+for (var i = 0; i < 15; i++)
+{
+    points[i] = new Point(i, i);
+    spoints[i] = new PointStruct(i + 100, i + 100);
+}
+
+foreach (var i in points)
+{
+    Console.WriteLine($"{i.x}, {i.y}");
+}
+foreach (var i in spoints)
+{
+    Console.WriteLine($"{i.x}, {i.y}");
+}
+
+class Point
+{
+    public int x, y;
+    public Point(int i, int j) 
+    {
+        x = i;
+        y = j;
+    }
+}
+
+struct PointStruct
+{
+    public int x, y;
+    public PointStruct(int i, int j)
+    {
+        x = i;
+        y = j;
+    }
+}
