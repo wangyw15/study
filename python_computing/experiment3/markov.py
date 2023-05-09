a2a06a0f27b962e4c4f7f2098490ef292baaac4f
wangyw15 <wangyanwei15@outlook.com>
2023-05-09 23:05:57 +0800

    马尔可夫链

--- python_computing/experiment3/markov.py
+++ python_computing/experiment3/markov.py
@@ -1,22 +1,21 @@
 import random
 import re
 
-def markov_analysis(text: str, n: int) -> dict[str, dict[str, int]]:
-    text = re.sub(r'[^A-Za-z ]', '', text).lower()
-    words = text.split()
-    substrings = [words[i: i+n] for i in range(int(len(words)/n)+1)]
+def markov_analysis(text: str, n: int) -> dict[str, dict[str, float]]:
+    # text = re.sub(r'[^A-Za-z ]', '', text).replace('\n', ' ').replace('\r', ' ').lower() # remove non-alphabetic characters
+    words = text.split() # split into words
+    phrases = [words[i: i+n] for i in range(int(len(words)/n)+1)] # split into phrases
 
     # transition matrix
-    transitions: dict[str, dict[str, int]] = {}
-    for i in range(len(substrings)-1):
-        current: str = ' '.join(substrings[i])
-        next: str = ' '.join(substrings[i+1])
-        if current not in transitions:
+    transitions: dict[str, dict[str, float]] = {}
+    for i in range(len(phrases)-1): # ignore last phrase
+        current: str = ' '.join(phrases[i])
+        next: str = ' '.join(phrases[i+1])
+        if current not in transitions.keys():
             transitions[current] = {}
-        if next not in transitions[current]:
+        if next not in transitions[current].keys():
             transitions[current][next] = 0
         transitions[current][next] += 1
-
     # frequency
     for current in transitions:
         total = sum(transitions[current].values())
@@ -25,20 +24,27 @@ def markov_analysis(text: str, n: int) -> dict[str, dict[str, int]]:
 
     return transitions
 
-def markov_generate(transistions: dict[str, dict[str, int]], max: int = 10):
+def generate_next(transistions: dict[str, dict[str, float]], current: str):
+    if current not in transistions.keys():
+        return None
+    options = transistions[current]
+    p = random.random()
+    choices = random.choices(list(options.keys()), weights=list(options.values()))
+    return choices[0]
+
+def markov_generate(transistions: dict[str, dict[str, float]], max_len: int = 2):
     phrases = list(transistions.keys())
-    n = len(phrases[0].split())
     current = random.choice(phrases)
-    result = current
-    for i in range(max):
-        options = transistions[current]
-        next = random.choices(list(options.keys()), list(options.values()))[0]
-        result = ' '.join(result.split() + next.split())
-        current = ' '.join(result.split()[-n:])
-    return result
+    results = [current]
+    for i in range(max_len):
+        current = generate_next(transistions, current)
+        if current is None:
+            break
+        results.append(current)
+    return ' '.join(results)
 
 if __name__ == '__main__':
-    n = 10
+    n = 1
     with open('data/emma.txt', 'r') as f:
         result = markov_analysis(f.read(), n)
-        print(markov_generate(result, 10))
+        print(markov_generate(result, 100))
