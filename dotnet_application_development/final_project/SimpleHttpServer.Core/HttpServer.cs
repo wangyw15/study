beb2c4c901a052197def13cc2c3d021bd6ea81e1
wangyw15 <wangyanwei15@outlook.com>
2023-05-11 20:45:59 +0800

    期末大作业 fxxk songbo

---
+++ dotnet_application_development/final_project/SimpleHttpServer.Core/HttpServer.cs
@@ -0,0 +1,80 @@
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleHttpServer.Core;
+
+struct HttpRequestMessage
+{
+    public string Method { get; set; }
+    public string Path { get; set; }
+    public string Version { get; set; }
+    public Dictionary<string, string> Headers { get; set; }
+
+    public static HttpRequestMessage ParseMessage(string msg)
+    {
+        var ret = new HttpRequestMessage();
+        ret.Headers = new Dictionary<string, string>();
+
+        var lines = msg.Split("\r\n");
+
+        // parse http basic info
+        var splitedFirstLine = lines[0].Split(' ');
+        ret.Method = splitedFirstLine[0];
+        ret.Path = splitedFirstLine[1];
+        ret.Version = splitedFirstLine[2][5..];
+
+        foreach (var line in lines[1..])
+        {
+            var splittedLine = line.Split(':');
+            var key = splittedLine[0].Trim();
+            var value = splittedLine[1].Trim();
+            if (ret.Headers.ContainsKey(key))
+            {
+                ret.Headers[key] = value;
+            }
+            else
+            {
+                ret.Headers.Add(key, value);
+            }
+        }
+
+        return ret;
+    }
+}
+
+struct HttpResponseMessage
+{
+    public string Version { get; set; }
+    public int StatusCode { get; set; }
+    public string StatusMessage { get; set; }
+    public Dictionary<string, string> Headers { get; set; }
+}
+
+class HttpServer
+{
+    protected TcpListener _listener;
+    protected string _wwwroot;
+
+    public HttpServer(string wwwroot, IPAddress addr, int port)
+    {
+        _wwwroot = wwwroot;
+        _listener = new TcpListener(addr, port);
+    }
+
+    public HttpServer(string wwwroot, IPEndPoint ep)
+    {
+        _wwwroot = wwwroot;
+        _listener = new TcpListener(ep);
+    }
+
+    public void Start()
+    {
+        _listener.Start();
+        //_listener.AcceptTcpClient()
+    }
+
+    public void Stop()
+    {
+        _listener.Stop();
+    }
+}
