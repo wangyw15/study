5ff21c8699c372c1f0ccf242e3f81fce8e78e3ac
wangyw15 <wangyanwei15@outlook.com>
2023-05-11 22:09:15 +0800

    多线程

--- dotnet_application_development/final_project/SimpleHttpServer.Console/Program.cs
+++ dotnet_application_development/final_project/SimpleHttpServer.Console/Program.cs
@@ -6,13 +6,13 @@ using HttpResponseMessage = SimpleHttpServer.Core.HttpResponse;
 var server = new HttpServer("./wwwroot", IPAddress.Loopback, 8080);
 server.OnRequest += (HttpRequest request) =>
 {
-	var ret = new HttpResponseMessage()
-	{
-		StatusCode = 200,
-		Content = "<html><body><h1>Hello World</h1></body></html>",
-		ContentType = "text/html"
-	};
-	return ret;
+    var ret = new HttpResponseMessage()
+    {
+        StatusCode = 200,
+        Content = "<html><body><h1>Hello World</h1></body></html>",
+        ContentType = "text/html"
+    };
+    return ret;
 };
 server.Start();
 Console.ReadLine();
--- dotnet_application_development/final_project/SimpleHttpServer.Core/HttpServer.cs
+++ dotnet_application_development/final_project/SimpleHttpServer.Core/HttpServer.cs
@@ -27,10 +27,10 @@ public struct HttpRequest
 
         foreach (var line in lines[1..])
         {
-			if (string.IsNullOrEmpty(line))
-			{
-				continue;
-			}
+            if (string.IsNullOrEmpty(line))
+            {
+                continue;
+            }
             var splittedLine = line.Split(':');
             var key = splittedLine[0].Trim();
             var value = splittedLine[1].Trim();
@@ -47,20 +47,20 @@ public struct HttpRequest
         return ret;
     }
 
-	public override string ToString()
-	{
-		var builder = new StringBuilder();
-		builder.Append($"{Method} {Path} {Version}\r\n");
-		if (Headers != null)
-		{
-			foreach(var header in Headers)
-			{
-				builder.Append($"{header.Key}: {header.Value}\r\n");
-			}
-		}
-		builder.Append("\r\n");
-		return builder.ToString();
-	}
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        builder.Append($"{Method} {Path} {Version}\r\n");
+        if (Headers != null)
+        {
+            foreach(var header in Headers)
+            {
+                builder.Append($"{header.Key}: {header.Value}\r\n");
+            }
+        }
+        builder.Append("\r\n");
+        return builder.ToString();
+    }
 }
 
 public struct HttpResponse
@@ -69,82 +69,88 @@ public struct HttpResponse
     public int StatusCode { get; set; }
     public string StatusMessage { get; set; }
     public string Content { get; set; }
-	public string ContentType { get; set; }
+    public string ContentType { get; set; }
 
-	public HttpResponse()
-	{
-		Version = "HTTP/1.1";
-		StatusCode = 200;
-		StatusMessage = "OK";
-		Content = "";
-		ContentType = "text/plain";
-	}
+    public HttpResponse()
+    {
+        Version = "HTTP/1.1";
+        StatusCode = 200;
+        StatusMessage = "OK";
+        Content = "";
+        ContentType = "text/plain";
+    }
 
-	public override string ToString()
-	{
-		var ret = new StringBuilder();
-		ret.Append($"{Version} {StatusCode} {StatusMessage}\r\n");
-		ret.Append($"Content-Length: {Content.Length}\r\n");
-		ret.Append($"Content-Type: {ContentType}\r\n");
-		ret.Append("\r\n");
+    public override string ToString()
+    {
+        var ret = new StringBuilder();
+        ret.Append($"{Version} {StatusCode} {StatusMessage}\r\n");
+        ret.Append($"Content-Length: {Content.Length}\r\n");
+        ret.Append($"Content-Type: {ContentType}\r\n");
+        ret.Append("\r\n");
         ret.Append(Content);
-		ret.Append("\r\n\r\n");
-		return ret.ToString();
-	}
+        ret.Append("\r\n\r\n");
+        return ret.ToString();
+    }
 }
 
 public class HttpServer
 {
-	public delegate HttpResponse RequestHandler(HttpRequest request);
-	public event RequestHandler? OnRequest;
+    public delegate HttpResponse RequestHandler(HttpRequest request);
+    public event RequestHandler? OnRequest;
     
-	protected TcpListener _listener;
+    protected TcpListener _listener;
     protected string _wwwroot;
 
-	public HttpServer(string wwwroot, IPAddress addr, int port)
+    public HttpServer(string wwwroot, IPAddress addr, int port)
     {
         _wwwroot = wwwroot;
         _listener = new TcpListener(addr, port);
-	}
+    }
 
     public HttpServer(string wwwroot, IPEndPoint ep)
     {
         _wwwroot = wwwroot;
         _listener = new TcpListener(ep);
-	}
+    }
 
-	public void Start()
+    public void Start()
     {
         _listener.Start();
         while (true)
-		{
-			var client = _listener.AcceptTcpClient();
-			var stream = client.GetStream();
-			
-			// read request
-			Span<byte> buffer = new byte[10240];
-			var length = stream.Read(buffer);
-			var msg = Encoding.UTF8.GetString(buffer[0..length]);
-			var request = HttpRequest.ParseMessage(msg);
-
-			// handle request
-			var response = new HttpResponse();
-			if (OnRequest != null)
-			{
-				response = OnRequest.Invoke(request);
-			}
-			// write response
-			var responseMsg = response.ToString();
-			stream.Write(Encoding.UTF8.GetBytes(responseMsg));
-			// close connection
-			stream.Flush();
-			client.Close();
-			buffer.Clear();
-		}
+        {
+            var client = _listener.AcceptTcpClient();
+            var task = new Task(() => ConnectionHander(client));
+            task.Start();
+        }
     }
 
     public void Stop()
     {
         _listener.Stop();
     }
+
+    protected void ConnectionHander(TcpClient client)
+	{
+        Console.WriteLine("New incoming request");
+		var stream = client.GetStream();
+		// read request
+		Span<byte> buffer = new byte[10240];
+		var length = stream.Read(buffer);
+		var msg = Encoding.UTF8.GetString(buffer[0..length]);
+		var request = HttpRequest.ParseMessage(msg);
+
+		// handle request
+		var response = new HttpResponse();
+		if (OnRequest != null)
+		{
+			response = OnRequest.Invoke(request);
+		}
+		// write response
+		var responseMsg = response.ToString();
+		stream.Write(Encoding.UTF8.GetBytes(responseMsg));
+		// close connection
+		stream.Flush();
+		client.Close();
+		buffer.Clear();
+	}
 }
