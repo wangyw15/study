2bdb724a9321645fbfab025d06d11d842383fd25
wangyw15 <wangyanwei15@outlook.com>
2023-05-11 18:09:41 +0800

    实验二

--- dotnet_application_development/week8/AnalysisAndDebug/Program.cs
+++ dotnet_application_development/week8/AnalysisAndDebug/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+abstract class A
+    {
+    int i;
+    abstract public void Print(string content);
+}
+class B:A
+{
+    int i;
+    protected string s;
+    public char ch;
+    public B(int j, string t, char d)
+    { 
+        i=j;
+        s=t; 
+        ch=d; 
+    }
+    public override void Print(string w)
+    { 
+        Console.WriteLine(w); 
+    }
+
+    public void Print(char ch)
+    {
+        Console.WriteLine(ch); 
+    }
+}
+class T
+{
+    public static void Main()
+    { 
+        B b=new B(11,"Here!",'f');
+        b.Print(b.ch); 
+    }
+}
