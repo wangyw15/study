d847d9f55244d2787638afe61c81e78e52ce6ca3
wangyw15 <wangyanwei15@outlook.com>
2023-09-13 10:36:27 +0800

    代码长得好看点

--- assembly_programming/experiment/week3/program.asm
+++ assembly_programming/experiment/week3/program.asm
@@ -9,29 +9,33 @@ start:
     mov ds, ax
 
     ; code starts here
-    mov bl, 0 ; for row
+    mov cl, 0 ; for row
     mov dl, 10h ; for char
+
     display:
     ; display character
     mov ah, 2
     int 21h
     ; new line
-    cmp bl, 10h
+    cmp cl, 10h
     je newline
     jmp without_newline
+
     newline:
     push dx ; save dx
     lea dx, crlf
     mov ah, 9
     int 21h
-    mov bl, 0 ; reset row counter
+    mov cl, 0 ; reset row counter
     pop dx ; restore dx
+
     without_newline:
-    inc bl ; increase row counter
+    inc cl ; increase row counter
     ; compare
     inc dl
     cmp dl, 80h
     jne display
+    
     ; return to dos
     mov al, 0
     mov ah, 4ch
