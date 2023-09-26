7b9059bb47165b1bc9de4fae638e2994b6b353ff
wangyw15 <wangyanwei15@outlook.com>
2023-09-27 00:56:07 +0800

    带bug的第五周实验

---
+++ assembly_programming/experiment/week5/README.md
@@ -0,0 +1,27 @@
+# 第五周实验 查找匹配字符串
+
+# 1. 实验目的
+
+查找匹配字符串SEARCH。 
+
+# 2. 实验要求
+
+程序接收用户键入的一个关键字以及一个句子。如果句子中不包含关键字则显示“No match!”；如果句子中包含关键字则显示“Match!”，且把该句子中的位置用十六进制数显示出来。
+
+## 实验结果
+
+要求程序的执行过程如下：
+
+```plain
+Enter keyword: abc
+Enter sentence: We are studying abc.
+Match at location: 11H of the sentence.
+Enter sentence: xyz, Ok?
+No match.
+Enter sentence: ^C
+```
+
+# 3. 实验报告要求
+
+1. 分析要点及调试后的正确程序。
+2. 实验体会。
---
+++ assembly_programming/experiment/week5/program.asm
@@ -0,0 +1,130 @@
+.model small
+.stack
+
+.data
+keyword_prompt db 'Enter keyword: $'
+sentence_prompt db 'Enter sentence: $'
+matched_prompt_prefix db 'Match at location: $'
+matched_prompt_suffix db 'H of the sentence.$'
+not_matched_prompt db 'No match.$'
+crlf db 0dh, 0ah, '$'
+keyword_buffer db 80, '?', 80 dup('$')
+sentence_buffer db 80, '?', 80 dup('$')
+
+.code
+start:
+mov ax, @data
+mov ds, ax
+
+begin:
+; get keyword
+mov dx, offset keyword_prompt
+mov ah, 9
+int 21h
+mov dx, offset keyword_buffer
+mov ah, 0ah
+int 21h
+
+call new_line_proc
+
+; get sentence
+mov dx, offset sentence_prompt
+mov ah, 9
+int 21h
+mov dx, offset sentence_buffer
+mov ah, 0ah
+int 21h
+
+call new_line_proc
+
+; search for keyword
+xor cx, cx ; reset cx
+sentence_loop:
+; ah for sentence, al for keyword, also applies to bx, cx
+mov si, offset sentence_buffer + 1
+add si, cx
+mov di, offset keyword_buffer + 1
+keyword_loop:
+inc si
+inc di
+mov bh, ds:[si]
+mov bl, ds:[di]
+cmp bh, bl
+jne char_not_matched
+; char matched
+; al for keyword location, ignore ah
+mov ax, di
+sub ax, offset keyword_buffer + 1
+cmp al, keyword_buffer + 1 ; check if al is at the end of keyword
+je fully_matched
+jmp keyword_loop
+char_not_matched:
+inc cx
+jmp sentence_loop
+
+mov dx, offset not_matched_prompt
+mov ah, 9
+int 21h
+call new_line_proc
+jmp begin
+
+fully_matched:
+; print matched location
+mov dx, offset matched_prompt_prefix
+mov ah, 9
+int 21h
+
+mov ax, cx
+mov cx, 8
+; if zero then print 0
+cmp ax, 0
+jne print_loop
+mov dl, '0'
+mov ah, 2
+int 21h
+jmp print_matched_prompt_suffix
+
+print_loop:
+; rol ax, 2
+rol ax, 1
+rol ax, 1
+; to ascii
+mov dl, al
+and dl, 03h
+cmp dl, 0
+je continue_print_loop
+cmp dl, 9
+jbe print_digit
+add dl, 'A' - 10
+jmp print_char
+print_digit:
+add dl, '0'
+print_char:
+mov ah, 2
+int 21h
+continue_print_loop:
+loop print_loop
+
+print_matched_prompt_suffix:
+mov dx, offset matched_prompt_suffix
+mov ah, 9
+int 21h
+
+call new_line_proc
+
+jmp begin
+mov ax, 4c00h
+int 21h
+
+new_line_proc proc near
+push ax
+push dx
+mov dx, offset crlf
+mov ah, 9
+int 21h
+pop dx
+pop ax
+ret
+new_line_proc endp
+
+end start
