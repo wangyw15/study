27f0c1ca5cf012c902cea2897108a5d435334b3b
wangyw15 <wangyanwei15@outlook.com>
2023-10-03 11:21:49 +0800

    开课

--- database_principle_1/final_project/SchoolManagementSystem/Models/OpeningCourse.cs
+++ database_principle_1/final_project/SchoolManagementSystem/Models/OpeningCourse.cs
@@ -1,6 +1,7 @@
 #pragma warning disable CS8618
 
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolManagementSystem.Models;
@@ -9,15 +10,19 @@ namespace SchoolManagementSystem.Models;
 [PrimaryKey(nameof(Semester), nameof(CourseId), nameof(TeacherId))]
 public class OpeningCourse
 {
+    [Required]
     [Column("xq")]
     public string Semester { get; set; }
 
+    [Required]
     [Column("kh")]
     public string CourseId { get; set; }
 
+    [Required]
     [Column("gh")]
     public int TeacherId { get; set; }
 
+    [Required]
     [Column("sksj")]
     public string Time { get; set; }
 }
