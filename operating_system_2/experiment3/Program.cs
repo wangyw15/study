a1d1d8ea1e1c92fa951a2993c418659ee6729cf0
wangyw15 <wangyanwei15@outlook.com>
2023-12-18 18:23:32 +0800

    实验三新增不同实存容量

--- operating_system_2/experiment3/Program.cs
+++ operating_system_2/experiment3/Program.cs
@@ -5,14 +5,21 @@ class Program
     static void Main()
     {
         var virtualSize = 32;
-        var physicalSize = 8;
-        
         var pages = Instruction.GeneratePageSequence(virtualSize);
 
-        var opt = new OptPaging();
-        Console.WriteLine($"OPT: {opt.Run(pages, physicalSize)}");
+        for (var i = 4; i < 33; i++)
+        {
+            var physicalSize = i;
 
-        var lru = new LRUPaging();
-        Console.WriteLine($"LRU: {lru.Run(pages, physicalSize)}");
+            Console.WriteLine($"Physical size: {physicalSize}");
+
+            var opt = new OptPaging();
+            Console.WriteLine($"OPT: {opt.Run(pages, physicalSize)}");
+
+            var lru = new LRUPaging();
+            Console.WriteLine($"LRU: {lru.Run(pages, physicalSize)}");
+
+            Console.WriteLine();
+        }
     }
 }
