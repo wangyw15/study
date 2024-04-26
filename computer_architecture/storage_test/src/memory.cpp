1defd57238a6ed7c04e917b559c4e2797517dd5b
wangyw15 <wangyanwei15@outlook.com>
2024-04-26 21:48:08 +0800

    内存读写测试

--- computer_architecture/storage_test/src/memory.cpp
+++ computer_architecture/storage_test/src/memory.cpp
@@ -11,23 +11,21 @@ constexpr unsigned int GB = 1 << 30;
 
 constexpr unsigned int ARRAY_SIZE = 1*GB;
 constexpr unsigned int ITERATIONS = 100;
-constexpr unsigned int THREADS = 16;
+constexpr unsigned int MAX_THREADS = 16;
 typedef char test_size_t;
 
 double calculate_speed(std::chrono::microseconds duration) {
     return ((double)ARRAY_SIZE / (double)GB) / ((double)duration.count() / (double)ITERATIONS / (double)1000000);
 }
 
-int main() {
-    test_size_t* arr = (test_size_t*)calloc(ARRAY_SIZE, sizeof(test_size_t));
-
+void write_test(test_size_t *arr, unsigned int thread_count) {
     std::vector<std::future<void>> futures;
-    futures.reserve(THREADS * sizeof(std::future<void>));
+    futures.reserve(thread_count * sizeof(std::future<void>));
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    for (unsigned int i = 0; i < THREADS; i++) {
-        auto length = ARRAY_SIZE / THREADS;
+    for (unsigned int i = 0; i < thread_count; i++) {
+        auto length = ARRAY_SIZE / thread_count;
         auto begin = arr + i * length;
         auto end = begin + length;
         futures.push_back(std::async([begin, end] {
@@ -42,11 +40,60 @@ int main() {
     }
 
     auto end = std::chrono::high_resolution_clock::now();
-    
+
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << calculate_speed(duration) << "GB/s" << std::endl;
+    std::cout << "write, "
+              << thread_count << ", "
+              << duration.count() << ", "
+              << calculate_speed(duration)
+              << std::endl;
+}
+
+void read_test(test_size_t *arr, unsigned int thread_count) {
+    std::vector<std::future<void>> futures;
+    futures.reserve(thread_count * sizeof(std::future<void>));
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (unsigned int i = 0; i < thread_count; i++) {
+        auto length = ARRAY_SIZE / thread_count;
+        auto begin = arr + i * length;
+        auto end = begin + length;
+        futures.push_back(std::async([begin, end] {
+            for (unsigned int j = 0; j < ITERATIONS; j++) {
+                for (auto it = begin; it != end; it++) {
+                    volatile auto val = *it;
+                }
+            }
+        }));
+    }
+
+    for (auto &future: futures) {
+        future.get();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "read, "
+              << thread_count << ", "
+              << duration.count() << ", "
+              << calculate_speed(duration)
+              << std::endl;
+}
+
+int main() {
+    std::cout << "type, threads (n), duration (microseconds), speed (GB/s)" << std::endl;
+    auto arr = (test_size_t*)calloc(ARRAY_SIZE, sizeof(test_size_t));
+
+    for (unsigned int threads = 1; threads <= MAX_THREADS; threads++) {
+        write_test(arr, threads);
+    }
+
+    for (unsigned int threads = 1; threads <= MAX_THREADS; threads++) {
+        read_test(arr, threads);
+    }
 
     delete[] arr;
-
     return 0;
 }
