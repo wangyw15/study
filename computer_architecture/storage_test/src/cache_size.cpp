9c971be7d221ca4b3ffd2b176613e9679df918ac
wangyw15 <wangyanwei15@outlook.com>
2024-05-05 20:55:07 +0800

    缓存测试修改

--- computer_architecture/storage_test/src/cache_size.cpp
+++ computer_architecture/storage_test/src/cache_size.cpp
@@ -1,18 +1,14 @@
 #include <array>
 #include <chrono>
 #include <iostream>
-#include <iterator>
-#include <limits>
 #include <random>
 
 constexpr unsigned int KB = 1 << 10;
 constexpr unsigned int MB = 1 << 20;
 
 constexpr unsigned int ITERATIONS = 1000;
-constexpr unsigned int STEP = 16 * KB;
-constexpr unsigned int MAX_DATA_SIZE = 1 * MB;
-
-constexpr char CHAR_MAX = std::numeric_limits<char>::max();
+constexpr unsigned int STEP = 128 * KB;
+constexpr unsigned int MAX_DATA_SIZE = 36 * MB;
 
 typedef char test_type;
 
@@ -27,29 +23,30 @@ int random_number(int max = CHAR_MAX) {
 double test_latency(int physical_size) {
     // initialize data
     auto array_size = physical_size / sizeof(test_type);
-    char data[array_size];
-    for (auto &i : data) {
-        i = random_number();
+    char *data = new char[array_size];
+    for (auto i = 0; i < array_size; i++) {
+        data[i] = random_number();
     }
 
     // initialize access array
     std::array<unsigned int, ITERATIONS> access;
-    for (auto &i : data) {
+    for (auto &i : access) {
         i = random_number(array_size);
     }
     
     // start
     auto start = std::chrono::high_resolution_clock::now();
-    for (auto &i : data) {
+    for (auto &i : access) {
         auto volatile tmp = data[i];
     }
     auto end = std::chrono::high_resolution_clock::now();
 
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    delete[] data;
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 }
 
 int main() {
-    std::cout << "size (byte), latency (microseconds)" << std::endl;
+    std::cout << "size (byte), latency (nanoseconds)" << std::endl;
     for (auto size = STEP; size <= MAX_DATA_SIZE; size += STEP) {
         std:: cout << size << ", " << test_latency(size) << std::endl;
     }
